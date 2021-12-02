280cfada17b03e4feb14e5f9635d3c22bb34b517
Angel Morcillo leganes <amorcill@1-A-1.42heilbronn.de>
2021-12-02 20:02:51 +0100

    Fix bug, segfault

--- stack_a_check_duplication.c
+++ stack_a_check_duplication.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   stack_check_duplication.c                          :+:      :+:    :+:   */
+/*   stack_a_check_duplication.c                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: amorcill <amorcill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/02 15:10:28 by amorcill          #+#    #+#             */
-/*   Updated: 2021/12/02 16:52:34 by amorcill         ###   ########.fr       */
+/*   Updated: 2021/12/02 19:41:24 by amorcill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,16 +20,21 @@ int		stack_a_check_duplication(t_push_swap *ps, t_stack *new_elemnt)
 
 	if (ps->stack_a == NULL)
 		return (1);
+	ft_printf("\n Cheking Duplicated numbers!\n");
 	actual = ps->stack_a;
 	i = 0;
-	while (i < ps->size_a)
+	while (i < ps->size_a && actual != NULL)
 	{
 		if (actual->nbr == new_elemnt->nbr)
+		{
+			ft_printf("\nDuplicated numbers!! %d\n", new_elemnt->nbr);
 			return (0);
-		actual = next;
-		if (next->next == NULL)
-			return (1);
-		next = next->next;
+		}
+		if (actual->next != NULL)
+		{
+			next = actual->next;			
+			actual = next;		
+		}
 		i++;
 	}
 	return (1);

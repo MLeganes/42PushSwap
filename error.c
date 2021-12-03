d47dd636f7a82557a0a0ff4a14d862e605835705
MLeganes <a.eme.ele@gmail.com>
2021-12-03 01:54:59 +0100

    Print error

--- error.c
+++ error.c
@@ -6,7 +6,7 @@
 /*   By: amorcill <amorcill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/01 16:15:53 by amorcill          #+#    #+#             */
-/*   Updated: 2021/12/01 20:00:08 by amorcill         ###   ########.fr       */
+/*   Updated: 2021/12/03 01:50:31 by amorcill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	error_print_exit(char *error_msg)
 {		
-	ft_putstr_fd("\n[Push Swap Error]\n", STDERR_FILENO);
+	ft_putstr_fd("Error\n", STDERR_FILENO);
 	ft_putstr_fd(error_msg, STDERR_FILENO);
 	ft_putstr_fd("\n", STDERR_FILENO);
 	exit (EXIT_FAILURE);

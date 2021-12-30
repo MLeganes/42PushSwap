d6106272b3bd7abfedb4ae23a45d6580b1798b61
Angel Morcillo leganes <amorcill@1-A-9.42heilbronn.de>
2021-12-30 20:26:17 +0100

    Comment system leaks and free

--- push_swap_free.c
+++ push_swap_free.c
@@ -6,7 +6,7 @@
 /*   By: amorcill <amorcill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/17 19:31:21 by amorcill          #+#    #+#             */
-/*   Updated: 2021/12/23 14:01:13 by amorcill         ###   ########.fr       */
+/*   Updated: 2021/12/30 20:12:42 by amorcill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,32 +14,32 @@
 
 void	push_swap_free(t_push_swap *ps)
 {
-	t_stack	*tmp;
+//	t_stack	*tmp;
 
-	while (ps->size_a > 0)
-	{
-		tmp = stack_a_remove_first(ps);
-		if (tmp != NULL)
-		{
-			free(tmp);
-			tmp = NULL;
-		}
-		ps->size_a--;
-	}	
-	while (ps->size_b > 0)
-	{
-		tmp = stack_b_remove_first(ps);
-		if (tmp != NULL)
-		{
-			free(tmp);
-			tmp = NULL;
-		}
-		ps->size_b--;
-	}	
+	// while (ps->size_a > 0)
+	// {
+	// 	tmp = stack_a_remove_first(ps);
+	// 	if (tmp != NULL)
+	// 	{
+	// 		free(tmp);
+	// 		tmp = NULL;
+	// 	}
+	// 	ps->size_a--;
+	// }	
+	// while (ps->size_b > 0)
+	// {
+	// 	tmp = stack_b_remove_first(ps);
+	// 	if (tmp != NULL)
+	// 	{
+	// 		free(tmp);
+	// 		tmp = NULL;
+	// 	}
+	// 	ps->size_b--;
+	// }	
 	if (ps != NULL)
 	{
-		free (ps->input_array);
-		free(ps);
+		//free (ps->input_array);
+		//free(ps);
 		ps = NULL;
 	}
 }
